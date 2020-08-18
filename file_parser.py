6287614633f9331a9dda94530cb90e87e23fb79e
chughsim <chughsim101@gmail.com>
2020-08-17 23:05:05 -0400

    Implemented file parsing

---
+++ file_parser.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+
+class FileParser():
+
+    def __init__(self, filelocation):
+        self.filelocation = filelocation
+        input_data = self.read_file()
+        print("File contents: {}".format(input_data))
+
+
+    def read_file(self):
+      file_contents = ""
+      with open(self.filelocation, "r") as f:
+         file_contents="".join(each_line.rstrip() for each_line in f)
+      return file_contents
+
+if __name__ == '__main__':
+   current_location = os.getcwd()
+   print ("Current working dir : %s" % current_location)
+   FileParser(current_location+'/phrases.txt')
